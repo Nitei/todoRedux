c59416a75e3007fe237fe700b6996df9ee1f925e
Jorge <jorgecampoy81@gmail.com>
2019-04-26 23:57:10 +0200

    agregados formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { environment } from '../environments/environment.prod';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     StoreModule.forRoot({todos: tareaReducer}),
     StoreDevtoolsModule.instrument({
--- src/app/todo/todo-add/todo-add.component.ts
+++ src/app/todo/todo-add/todo-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducers';
+import * as Acciones from '../todo.actions';
+
 
 @Component({
   selector: 'app-todo-add',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoAddComponent implements OnInit {
 
-  constructor() { }
+  txtInput: FormControl;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.txtInput = new FormControl('', Validators.required)
+  }
+
+  agregarTarea() {
+    if (this.txtInput.valid) {
+      const accion = new Acciones.AgregarTareaAction(this.txtInput.value);
+      this.store.dispatch(accion);
+      this.txtInput.setValue('')
+    }
   }
 
 }
