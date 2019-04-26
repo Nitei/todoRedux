d059b3f734251582e582a481658cae142c5d73e0
Jorge <jorgecampoy81@gmail.com>
2019-04-26 23:43:59 +0200

    agregada ngrx-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { TodoItemComponent } from './todo/todo-item/todo-item.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { tareaReducer } from './todo/todo.reducer';
+import { environment } from '../environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,12 @@ import { TodoAddComponent } from './todo/todo-add/todo-add.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({todos: tareaReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
